79fda76940ebd39d1d3eaa570a4c820eeeb8a7d9
agnyp <andreas@gnyp.de>
2013-01-14 11:37:08 +0100

    Update bestpractice/isNumber.js
    add url

--- bestpractice/isNumber.js
+++ bestpractice/isNumber.js
@@ -1,6 +1,9 @@
 /**
  * This functions is able to determine very robustly if a given
  * string is a number, or not
+ * For a very detailed explanation and exhausting tests go to 
+ * 
+ * http://dl.dropbox.com/u/35146/js/tests/isNumber.html (where I found it)
  */
 function isNumber(n){
   return !isNaN(parseFloat(n)) && isFinite(n)
